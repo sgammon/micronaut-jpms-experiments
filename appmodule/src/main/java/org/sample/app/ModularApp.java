c19c3581f8df35a59ef28ccfa6bfb6170d063465
Sam Gammon <sam@elide.ventures>
2024-03-09 14:27:04 -0800

    additional tests
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- appmodule/src/main/java/org/sample/app/ModularApp.java
+++ appmodule/src/main/java/org/sample/app/ModularApp.java
@@ -19,9 +19,55 @@ public class ModularApp {
         }
     }
 
+    private static String readResourceFromModule(Module base, String name) {
+        var thisModule = ModularApp.class.getModule();
+        assert base.isNamed();
+        assert base.getName().equals("demo.libmodule");
+        assert base.isExported("org.sample.lib");
+        assert base.canRead(thisModule);
+
+        try (var stream = thisModule.getResourceAsStream(name)) {
+            assert stream != null;
+
+            try (var buf = new BufferedReader(new InputStreamReader(stream))) {
+                return buf.readLine();
+            }
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    private static String readResourceThisModule(String name) {
+        var thisModule = ModularApp.class.getModule();
+
+        try (var stream = thisModule.getResourceAsStream(name)) {
+            assert stream != null;
+
+            try (var buf = new BufferedReader(new InputStreamReader(stream))) {
+                return buf.readLine();
+            }
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, modular Java! " + ModularLibClass.getMessage());
         System.out.println("App resource: " + readResource("/appmodule.txt"));
-        System.out.println("App resource: " + readResource("/libmodule.txt"));
+        System.out.println("Lib resource: " + readResource("/libmodule.txt"));
+        System.out.println("App resource (T): " + readResourceFromModule(
+            ModularLibClass.class.getModule(),
+            "/META-INF/micronaut/appmodule.txt"
+        ));
+        System.out.println("Lib resource (T): " + readResourceFromModule(
+            ModularLibClass.class.getModule(),
+            "/META-INF/micronaut/libmodule.txt"
+        ));
+        System.out.println("App resource (T): " + readResourceThisModule(
+            "/META-INF/micronaut/appmodule.txt"
+        ));
+        System.out.println("Lib resource (T): " + readResourceThisModule(
+            "/META-INF/micronaut/libmodule.txt"
+        ));
     }
 }
